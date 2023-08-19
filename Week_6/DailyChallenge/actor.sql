8d171143f7d1e7a9c34a79f519f29af962014380
Astaromuzhev <annastaromuzhev@gmail.com>
2023-08-19 21:56:56 +0300

    week6 day2 DC

---
+++ Week_6/DailyChallenge/actor.sql
@@ -0,0 +1,8 @@
+SELECT count(1) FROM actor;
+SELECT * FROM actor;
+INSERT INTO actor (first_name)
+VALUES ('Anna');
+INSERT INTO actor (lat_name)
+VALUES ('Staromuzhev');
+INSERT INTO actor (first_name, last_name)
+VALUES ('Anna','Staromuzhev');
